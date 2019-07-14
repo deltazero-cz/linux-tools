0345f69fa965f8881f84343aeb69316f814010b3
ΔO <david@deltazero.cz>
2019-07-14 17:44:14 +0200

    + chroot for rsync setup

--- server/setup.chroot.for.rsync.sh
+++ server/setup.chroot.for.rsync.sh
@@ -8,8 +8,8 @@
 echo -n "Is this the correct chroot dir: $PWD [Y/n] "
 read answer
 if [[ ! -z $answer ]] && [[ $answer != 'Y' ]] && [[ $answer != 'y' ]]; then
-	echo "Abort."
-	exit 1
+  echo "Abort."
+  exit 1
 fi
 
 echo
@@ -17,12 +17,12 @@ echo "Doing magic..."
 
 copies=$((ldd `which sh`; ldd `which bash`; ldd `which rsync`) | awk '/\// { print ($3 ? $3 : $1) }' | sort | uniq)
 copies+=" $(which sh) $(which bash) $(which rsync)"
-# echo $copies; exit;
+# echo $copies; exit
 
 for f in $copies; do
-	d=$(dirname ${f})
-	[[ ! -d ".$d" ]] && sudo mkdir -p ".$d"
-	[[ ! -f ".$f" ]] && sudo cp -v "$f" ".$f"
+  d=$(dirname ${f})
+  [[ ! -d ".$d" ]] && sudo mkdir -p ".$d"
+  [[ ! -f ".$f" ]] && sudo cp -v "$f" ".$f"
 done
 
 echo
